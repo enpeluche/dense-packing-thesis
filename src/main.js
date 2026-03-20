797c6cd2d194eee8cf602a4f226c229e126effc6
Lucas Petit <lucas.noirotorion@gmail.com>
2026-03-20 15:26:26 +0100

    minor fix

--- src/main.js
+++ src/main.js
@@ -18,8 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
   initCarousel();
 });
 
-// On détecte si on est en local ou sur GitHub Pages
 const isProduction = window.location.hostname.includes("github.io");
 
-// Si Prod : on ajoute le nom du repo. Si Local : on reste à la racine.
-export const APP_ROOT = isProduction ? "/lattice-reduction-internship/" : "/";
+export const APP_ROOT = isProduction ? "/dense-packing-thesis/" : "/";
